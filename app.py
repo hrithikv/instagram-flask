45011bfb99ac9efbf5f6fe8b7820edc8d78107da
nakajiii <think0224growrich0729@gmail.com>
2018-11-29 19:16:00 +0900

    Add url

--- app.py
+++ app.py
@@ -23,12 +23,15 @@ def confirm():
         soup = BeautifulSoup(html.text, 'lxml')
         entryDict = json.loads(str(soup.select_one('body > script:nth-of-type(1)').text).split('=', 1)[1][1:-1])
 
-        texts = []
-        for i in entryDict['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']:
-            texts.append(i['node']['edge_media_to_caption']['edges'][0]['node']['text'])
+        data = entryDict['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']
+        '''
+        texts.append(i['node']['edge_media_to_caption']['edges'][0]['node']['text'])
+        likes.append(i['node']['edge_liked_by'])
+        images.append(i['node']['display_url'])
+        '''
+        homeUrl = '/form'
 
-
-        return render_template("confirm.html", texts=texts)
+        return render_template("confirm.html", data=data, homeUrl=homeUrl)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
